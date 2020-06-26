bb5b09418b03992edc4ce7ed5c71209053b93297
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-06-25 21:38:02 -0500

    Updates

---
+++ precache-manifest.818a324dd261e6f66c59224d6e63b8c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62744ec3037f6d82527a72f64a1b2059",
+    "url": "/my-react-gh-page/index.html"
+  },
+  {
+    "revision": "1b5a924bd08384f4647f",
+    "url": "/my-react-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "093d344660ff90b2d7e5",
+    "url": "/my-react-gh-page/static/js/2.3574102e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-gh-page/static/js/2.3574102e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5a924bd08384f4647f",
+    "url": "/my-react-gh-page/static/js/main.80316d72.chunk.js"
+  },
+  {
+    "revision": "82e0a78a8e00dbb0a283",
+    "url": "/my-react-gh-page/static/js/runtime-main.0d878cde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
